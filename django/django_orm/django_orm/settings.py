02dc0548cde203f15bed1275e05881afdbea4bec
srustith8 <srustith8@gmail.com>
2021-05-13 16:52:23 +0530

    how to add a model for a database view is added

--- django/django_orm/django_orm/settings.py
+++ django/django_orm/django_orm/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'orm_app',
 ]
 
+# AUTH_USER_MODEL='orm_app.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- django/django_orm/orm_app/models.py
+++ django/django_orm/orm_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
-
+from django.contrib.auth.models import User,AbstractUser
+from django.conf import settings
 # Create your models here.
 
 class UserParent(models.Model):
@@ -22,4 +22,26 @@ class Article(models.Model):
 
     class Meta:
         ordering = ('headline',)
-        
+
+# class Tweet(models.Model):
+#     tweet = models.TextField()
+#     favourite = models.ManyToManyField('orm_app.User', blank=True, related_name='user_favourite')
+
+#     def __unicode__(self):
+#         return self.tweet
+
+# class User(AbstractUser):
+#     tweet = models.ManyToManyField(Tweet, blank=True)
+#     follower = models.ManyToManyField('orm_app.User', blank=True)
+#     pass
+
+
+#How to add a model for a database view?
+class TempUser(models.Model):
+    first_name = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False 
+        db_table = "temp_user"
+
+
